30d557fb1e35d063f7bc8e600a14b53632db542b
vireobloom88 <vireobloom88@gmail.com>
2026-03-19 05:33:22 +0900

    fix:word

--- app/cat-image.tsx
+++ app/cat-image.tsx
@@ -2,7 +2,7 @@
  
 import { useState } from "react";
 import { fetchImage } from "./fetch-image";
-import styles from "./page.module.css"; // 追加
+import styles from "./page.module.css";
  
 type CatImageProps = {
   url: string;
@@ -12,7 +12,7 @@ export function CatImage({ url }: CatImageProps) {
   const [imageUrl, setImageUrl] = useState<string>(url);
  
   const refreshImage = async () => {
-    setImageUrl(""); // 初期化
+    setImageUrl("");
     const image = await fetchImage();
     setImageUrl(image.url);
   };
@@ -20,7 +20,7 @@ export function CatImage({ url }: CatImageProps) {
   return (
     <div className={styles.page}>
       <button onClick={refreshImage} className={styles.button}>
-        他のにゃんこも見る
+        One more cat!
       </button>
       <div className={styles.frame}>
         {imageUrl && <img src={imageUrl} className={styles.img} />}
