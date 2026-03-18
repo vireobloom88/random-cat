921be60931ffa51012bb3ccd40c7860620880104
vireobloom88 <vireobloom88@gmail.com>
2026-03-19 04:19:51 +0900

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { connection } from "next/server";
+import { CatImage } from "./cat-image"; // 追加
+import { fetchImage } from "./fetch-image";
+ 
+export default async function Home() {
+  // ビルド時にfetchImageの結果が固定されないようにする
+  await connection();
+  // APIから画像を取得
+  const image = await fetchImage();
+  // 画像のURLを渡す
+  return <CatImage url={image.url} />;
+}
